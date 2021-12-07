01e0f8cfcee0d4d8c6910e0e428d5ebf1e35b8d9
Yanuar <yanyanwhy12@gmail.com>
2021-12-07 14:54:48 +0700

    Tugas PHP

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "php2";
+
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    //  if($conn){
+    //    echo "koneksi berhasil";
+    //  }else{
+    //    echo "koleksi gagal";
+    //  }
+?>
